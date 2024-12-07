789b43d405b8cb537e4cab900dde84a1fa9e0fc5
Dương Gia Bảo <146315726+GBao06@users.noreply.github.com>
2024-12-07 10:24:15 +0700

    Update OOP.py

--- OOP.py
+++ OOP.py
@@ -1,48 +1,60 @@
 class CongNhan:
     def __init__(self,MaCN,Hoten,Bac,SNLV,NKHD) :
-        self.MaCN = MaCN
-        self.Hoten = Hoten
-        self.Bac = Bac
-        self.SNLV = SNLV
-        self.NKHD = NKHD
+        self.__MaCN = MaCN
+        self.__Hoten = Hoten
+        self.__Bac = Bac
+        self.__SNLV = SNLV
+        self.__NKHD = NKHD
+    def set_MaCN(self,MaCN):
+        self.__MaCN = MaCN
+    def set_Hoten(self,Hoten):
+        self.__Hoten = Hoten
+    def set_Bac(self,Bac):
+        self.__Bac = Bac   
+    def set_SoNgayLamViec(self,SNLV):
+        self.__SNLV = SNLV   
+    def set_NgayKiHopDong(self,NKHD):
+        self.__NKHD = NKHD           
     def get_MaCN(self):
-        return self.MaCN
+        return self.__MaCN
     def get_HoTen(self):
-        return self.Hoten
+        return self.__Hoten
     def get_Bac(self):
-        return self.Bac
+        return self.__Bac
     def get_SoNgayLamViec(self):
-        return self.SNLV
+        return self.__SNLV
     def get_NgayKiHopDong(self):
-        return f"{self.NKHD["ngay"]}/{self.NKHD["thang"]}/{self.NKHD["nam"]}"
+        return f"{self.__NKHD["ngay"]}/{self.__NKHD["thang"]}/{self.__NKHD["nam"]}"
     
     def __str__(self):
         return(
-            f"Ma cong nhan: {self.MaCN}\n"
-            f"Ho ten: {self.Hoten}\n"
-            f"Bac: {self.Bac}\n"
-            f"So ngay lam viec: {self.SNLV}\n"
-            f"Ngay ki hop dong:  {self.NKHD["ngay"]}/{self.NKHD["thang"]}/{self.NKHD["nam"]}\n"
+            f"Ma cong nhan: {self.__MaCN}\n"
+            f"Ho ten: {self.__Hoten}\n"
+            f"Bac: {self.__Bac}\n"
+            f"So ngay lam viec: {self.__SNLV}\n"
+            f"Ngay ki hop dong:  {self.__NKHD["ngay"]}/{self.__NKHD["thang"]}/{self.__NKHD["nam"]}\n"
             f"Tien luong: {self.get_tienluong()}"
-        )
+        )    
     def display(self):
-        print(f"{self.MaCN},{self.Hoten},{self.SNLV},{self.get_tienluong()}")
+        print(f"{self.__MaCN},{self.__Hoten},{self.__SNLV},{self.get_tienluong()}")
 
     def get_tienluong(self):
-        if self.Bac == 1:
-            return self.SNLV * 300000
-        elif self.Bac == 2:
-            return self.SNLV * 350000
-        elif self.Bac == 3:
-            return self.SNLV * 400000
+        if self.__Bac == 1:
+            return self.__SNLV * 300000
+        elif self.__Bac == 2:
+            return self.__SNLV * 350000
+        elif self.__Bac == 3:
+            return self.__SNLV * 400000
+        else: return 300000
+        
     def __gt__(self, other):
-        if self.NKHD["nam"] > other.NKHD["nam"]: 
+        if self.__NKHD["nam"] > other.__NKHD["nam"]: 
             return True
-        elif self.NKHD["nam"] == other.NKHD["nam"]:
-            if self.NKHD["thang"] > other.NKHD["thang"]:
+        elif self.__NKHD["nam"] == other.__NKHD["nam"]:
+            if self.__NKHD["thang"] > other.__NKHD["thang"]:
                 return True
-            elif self.NKHD["thang"] == other.NKHD["thang"]:
-                return self.NKHD["ngay"] > other.NKHD["ngay"]
+            elif self.__NKHD["thang"] == other.__NKHD["thang"]:
+                return self.__NKHD["ngay"] > other.__NKHD["ngay"]
         return False
 
 arr = []
